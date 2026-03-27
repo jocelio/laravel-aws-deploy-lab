2b379f5f96f24f48d88cb71ed18107db6655898b
Jocélio Lima <jocelio27@gmail.com>
2026-03-27 16:09:56 -0300

    test deploy

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json([
         'app' => 'Laravel AWS Deploy Lab',
+        'deploy' => 'teste de deploy',
         'version' => env('APP_VERSION'),
         'instance' => gethostname(),
     ]);
