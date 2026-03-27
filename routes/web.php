c55ff6ffe5369d7053e54b5c33dcd322cdcef98b
Jocélio Lima <jocelio27@gmail.com>
2026-03-27 18:39:36 -0300

    change message

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json([
         'app' => 'Laravel AWS Deploy Lab',
-        'deploy' => 'teste de deploy',
+        'deploy' => 'Teste de deploy aws ec2 pull',
         'version' => env('APP_VERSION'),
         'instance' => gethostname(),
     ]);
