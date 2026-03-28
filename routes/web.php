af0d58ec71612bab3a9d73d02cde4403e9af132c
Jocélio Lima <jocelio27@gmail.com>
2026-03-28 10:23:20 -0300

    add health check endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json([
         'app' => 'Laravel AWS Deploy Lab',
-        'deploy' => 'Teste de deploy aws ec2 pull',
+        'deploy' => 'v2',
         'version' => env('APP_VERSION'),
         'instance' => gethostname(),
     ]);
-});
+ });
+
+ Route::get('/health', fn() => response()->json(['ok' => true]));
